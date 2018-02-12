b86178fc19ecf75b9f385564dd22c6056cb3601f
Srujana <Srujana@DESKTOP-HMVA6KA>
2018-02-12 16:48:43 +0530

    project created

---
+++ hook_up_BackEnd/src/main/java/com/hook_up_BackEnd/App.java
@@ -0,0 +1,13 @@
+package com.hook_up_BackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
